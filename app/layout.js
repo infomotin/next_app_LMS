0fedfb4855316c537daf78d831eec4885e4b4145
infomotin <motin.mmk.jr@gmail.com>
2025-02-12 11:32:50 +0600

    onclick

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
         )}
       >
         {children}
+        <Toaster richColors position="top-right" />
       </body>
     </html>
   );
--- components/ui/TestText.jsx
+++ components/ui/TestText.jsx
@@ -1,12 +1,21 @@
+ 'use client';
  import React from 'react';
  import {Button} from './button';
  import {cn} from '@/lib/utils';
+ import { toast } from 'sonner';
  const TestText = () => {
-    return (
-        <div className={cn('flex items-center justify-center h-screen')}>
-            <Button className="bg-red-500 text-white" variant="secondary">Test Button</Button>
-        </div>
-    );
+    const clickHandler = (mode) => {
+        if(mode) {
+            toast.success('Success')
+        } else {
+            toast.error('Error')
+        }
+    }
+     return (
+         <div className={cn('flex items-center justify-center h-screen')}>
+             <Button className="bg-red-500 text-white" variant="default" onClick={() => clickHandler(false)}>Test Button</Button>
+         </div>
+     );
  };
  
  export default TestText;
