d56bf715e8edcb07d2cbea4483dcbd04f488bfaa
infomotin <motin.mmk.jr@gmail.com>
2025-02-16 11:10:12 +0600

    home design

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import SiteFooter from '@/components/site-footer';
 
 const MainLayout = ({ children }) => {
     return (
         <div>
             <main>{children}</main>
+            <SiteFooter />
         </div>
     );
 };
--- components/support.jsx
+++ components/support.jsx
@@ -4,19 +4,28 @@ import  Image from 'next/image';
 
 const Support = () => {
     return (
-        <div className="bg-darkBlue main-h-screen px-2 py-12">
-            <div className='w-full bg-blue-50 p-6 flex flex-col md:flex-row items-center pt-12 pb-10 pl-12 pr-12 justify-center'> 
-                <div className='md:w-1/2 text-center md:text-left pt-5 md:pt-0 pb-10 md:pb-0'>
-                    <Image src="/assets/images/one.png" alt="element" width={400} height={400} className='rounded-lg'/>
-                </div>
-                <div className='md:w-1/2 text-center md:text-left pt-5 md:pt-0 pb-10 md:pb-0'>
-                    <h3 className="text-blue-600 font-semibold text-lg md:text-3xl mb-2">Step By Step Guide</h3>
-                    <h2 className='text-3xl font-bold text-gray-800 font-heading md:text-6xl pb-10 md:pb-0 mb-2'>
-                        Put Your Learning To Practice 
-                    </h2>
-                    <p className='text-gray-500 font-semibold text-lg mb-4 px-12 text-justify text-muted-foreground'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        <div className="bg-darkBlue text-black py-5 px-5 md:py-6 md:px-6 border-b border-gray-900 " >
+            <div className='max-w-7xl mx-auto flex flex-col md:flex-row intems-center space-y-12 md:space-y-0 md:space-x-12 '>
+                <div className='flex-1'>
+                    <p className='mt-5 text-3xl font-bold leading-tight text-gray-900 font-popins'>
+                        <span className="relative inline-flex sm:inline">
+                            <span className="bg-gradient-to-r from-[#44bcff] via-[#ff44ec] to-[#ff675e] blur-lg filter opacity-30 w-full h-full absolute inset-0">
+                               
+                            </span>
+                            <span className='relative'>Let us know for any support</span>
+                        </span>
                     </p>
+                    <p className='text-black leading-relaxed mt-8 mb-8'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+                        more text Best support team you can your choise 
+                    </p>
+                    <div className='flex flex-wrap gap-4'>
+                    <a href="#" className='bg-fuchsia-500 text-white font-semibold px-6 py-3 rounded-lg shadow hover:bg-fuchsia-800 transition'>Contact us</a>
+                    <a href="#" className='bg-gray-500 text-white font-semibold px-6 py-3 rounded-lg shadow hover:bg-gray-800 transition'>Call For Support</a>
+                    </div>
+                </div>
+                <div className='flex-1 flex justify-center items-center'>
+                <Image src="/assets/images/support.png" alt="element" width={400} height={400} className='rounded-lg'/>    
                 </div>
             </div>
         </div>
