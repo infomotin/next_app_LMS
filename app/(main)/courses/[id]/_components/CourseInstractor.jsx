b7947beea718a473049574bfe25ab1fd2c2b475e
infomotin <motin.mmk.jr@gmail.com>
2025-02-19 16:37:35 +0600

    all new in instractor

--- app/(main)/courses/[id]/_components/CourseInstractor.jsx
+++ app/(main)/courses/[id]/_components/CourseInstractor.jsx
@@ -3,29 +3,34 @@ import { Presentation } from "lucide-react";
 import { UsersRound } from "lucide-react";
 import { Star } from "lucide-react";
 import { MessageSquare } from "lucide-react";
+import Image from 'next/image';
 const CourseInstractor = ({course}) => {
+    console.log(course?.instructor);
+    const fullName = course?.instructor?.firstName + " " + course?.instructor?.lastName;
     return (
         <div className="bg-gray-50 rounded-md p-8">
                             <div className="md:flex md:gap-x-5 mb-8">
                                 <div className="h-[310px] w-[270px] max-w-full  flex-none rounded mb-5 md:mb-0">
-                                    <img
-                                        src="https://avatars.githubusercontent.com/u/47516621?v=4"
-                                        alt=""
+                                    <Image
+                                        src={course?.instructor?.profilePicture}
+                                        alt={fullName}
                                         className="w-full h-full object-cover rounded"
+                                        width={270}
+                                        height={310}
                                     />
                                 </div>
                                 <div className="flex-1">
                                     <div className="max-w-[300px]">
                                         <h4 className="text-[34px] font-bold leading-[51px]">
-                                            Kazi Ariyan
+                                            {fullName}
                                         </h4>
                                         <div className="text-gray-600 font-medium mb-6">
-                                            Senior Software Engineer
+                                            {course?.instructor?.designation}
                                         </div>
                                         <ul className="list space-y-4">
                                             <li className="flex items-center space-x-3">
                                                 <Presentation className="text-gray-600" />
-                                                <div>10+ Courses</div>
+                                                <div>10+ Courses</div> 
                                             </li>
                                             <li className="flex space-x-3">
                                                 <UsersRound className="text-gray-600" />
@@ -44,13 +49,7 @@ const CourseInstractor = ({course}) => {
                                 </div>
                             </div>
                             <p className="text-gray-600">
-                                There are many variations of passages of Lorem Ipsum
-                                available, but the majority have suffered alteration in some
-                                form, by injected humour, or randomised words which do not
-                                look even slightly believable. If you are going to use a
-                                passage of Lorem Ipsum, you need to be sure there is not
-                                anything embarrassing hidden in the middle of text. All the
-                                Lorem Ipsum generators on the Internet tend.
+                                {course?.instructor?.bio}
                             </p>
                         </div>
     );
--- queries/courses.js
+++ queries/courses.js
@@ -4,6 +4,7 @@ import {User} from "@/model/users-model";
 import {Testimonial} from "@/model/testimonial-model";
 import {Module} from "@/model/modules-model";
 import {replaceMongoIdInArray,replaceMongoIdInObject} from "@/lib/convertData";
+import { getEnrollmentsForCourse } from "./enrollments";
 export async function getCourses() {
     try {
         const courses = await Course.find({}).select(["title", "subtitle", "thumbnail","modules","instructor","category","price"])
@@ -58,4 +59,17 @@ export async function getCoursesDetails(id) {
     } catch (error) {
         console.log(error);
     } 
+}
+
+
+
+export async function getCourseDetailsByInstructor(instructorId){
+    const courses = await Course.find({instructor: instructorId}).lean();
+
+    const enrollments = await Promise.all(courses.map(async (course) => {
+        const enrollment = await getEnrollmentsForCourse(course._id.toString());
+        return replaceMongoIdInArray(enrollment);
+    }));
+
+    return { courses, enrollments };
 }
