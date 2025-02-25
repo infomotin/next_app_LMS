672d4c1f7f8b877081242781e3b8651d72992862
infomotin <motin.mmk.jr@gmail.com>
2025-02-25 20:49:09 +0600

    strip pay

--- app/actions/stripe.js
+++ app/actions/stripe.js
@@ -1,5 +1,53 @@
 "use server"
+import { headers } from "next/headers";
+const CURRENCY = "USD";
+import { formatAmountForStripe } from "@/lib/stripe-helpers";
+import { stripe } from "@/lib/stripe";
+export async function createCheckoutSession(data) {
 
-export async function createCheckoutSession(data){
+    const ui_mode = "hosted";
+    const origin = (await headers()).get("origin");
+    const checkoutSession = await stripe.checkout.sessions.create({
+        mode: "payment",
+        submit_type: "auto",
+        line_items: [
+            {
+                quantity: 1,
+                price_data: {
+                    currency: CURRENCY,
+                    product_data: {
+                        name: "EASYLEARNINGs",
+                    },
+                    unit_amount: formatAmountForStripe(120, CURRENCY),
+                },
+            },
+        ],
+        ...(ui_mode === "hosted" && {
+            success_url: `${origin}/enroll-success?session_id={CHECKOUT_SESSION_ID}&courseId=123456`,
+            cancel_url: `${origin}/courses`,
+        }),
+        ui_mode
+
+    });
+    return {
+        client_secret: checkoutSession.client_secret,
+        url: checkoutSession.url,
+    };
+
+
 
 }
+
+
+export async function createPaymentIntent(data) {
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: formatAmountForStripe(120,
+            CURRENCY
+        ),
+        automatic_payment_methods: { enabled: true },
+        currency: CURRENCY
+    });
+    return { client_secret: paymentIntent.client_secret };
+
+}
+/// End Method 
--- components/enroll-course.jsx
+++ components/enroll-course.jsx
@@ -1,12 +1,14 @@
+
+"use client";
 import React from "react";
 import { ArrowRight } from "lucide-react";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import {createCheckoutSession} from "@/app/actions/stripe";
-
 const EnrollCourse = ({ asLink }) => {
     const formAction = async(data)=>{
         const {url} = await createCheckoutSession(data);
+        window.location.assign(url);
     }
     return (
         <>
